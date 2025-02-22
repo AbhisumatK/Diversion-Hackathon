764f09c7faedc0e10f8dd81f53ece52d96fee77a
ArijitKRoy <83859073+arijitkroy@users.noreply.github.com>
2025-02-22 11:38:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,4 +17,14 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import r2_score, accuracy_score
 
-st.title("Ez-ViZ")
+uploaded_file = st.file_uploader("Upload a CSV File", type=[ "csv", "tsv", "xlsx", "json", "parquet", "pkl", "h5", "xml" ])
+if uploaded_file is not None:
+    # Load the uploaded file
+    df = pd.read_csv(uploaded_file)
+    st.write("### Data Preview")
+    st.write(df.head())
+
+    num_features = df.select_dtypes(include=[np.number]).columns.tolist()
+    cat_features = df.select_dtypes(exclude=[np.number]).columns.tolist()
+
+    
