9006640efc6fd54be38257f5be4474497c53dcee
ArijitKRoy <83859073+arijitkroy@users.noreply.github.com>
2025-02-22 11:48:14 +0530

    .

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ if uploaded_file is not None:
     cat_features = df.select_dtypes(exclude=[np.number]).columns.tolist()
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     st.write("### Column Analysis")
     st.write("**Numerical Features:**", num_features)
     st.write("**Categorical Features:**", cat_features)
@@ -65,5 +66,7 @@ if uploaded_file is not None:
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
